24adae7b9c815a299064ee4ed9cd8e60631cbb83
Cassiano pereira braz <cassiano.braz@PORTOVELHO.RO.GOV.BR>
2025-02-14 12:42:28 -0400

    feat progressBar with time

--- frontend/src/view/Song.jsx
+++ frontend/src/view/Song.jsx
@@ -51,6 +51,7 @@ export const Song = () => {
           duration={duration}
           randomIdFromArtist={randomIdFromArtist}
           randomId2FromArtist={randomId2FromArtist}
+          audio={audio}
         />
         <div>
           <p className="song__name">{name}</p>
